0f19b3c74f49121a9bede461b204416fcf01f723
mestrak <mestrak@github.com>
2022-05-25 12:37:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# single-spa-parcel-example
+
+This project is an example of web app built with multiple microfrontends running in different VM's using the [single-spa](https://github.com/CanopyTax/single-spa) framework.
+
+## To run this example in development mode use: 
+1. Have a Mongo DB running in port **27017**
+2. Inside each folder (sample-api, sample-api2, sample-vue, sample-react, sample-single-spa) run:
+```
+npm install
+npm run serve
+```
+3. You can access the portal at [http://localhost:5000/index.local](http://localhost:5000/index.local)
+
+## To run this example in production use:
+### inside the environment folder:
+```
+docker-compose build
+docker-compose up
+```
+You can access the portal at [http://localhost:5001](http://localhost:5001)
---
+++ sample-vue/src/components/ParcelPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="py-3">
+    <h1 class="h1-vue">I am an application</h1>
+      <!-- <input type="text" placeholder="Filter by department or employee" v-model="filter" /> -->
+
+    <div id="parcel"></div>
+    <br /><br />
+    <div id="parcel2"></div>
+
+  </div>
+</template>
+
+<script>
+//import { mountRootParcel } from 'single-spa';
+import { mountParcel } from '../main.js'
+
+export default {
+  name: 'parcelPage',
+  mounted() {
+      this.loadParcel()
+  },
+  methods: {
+      loadParcel() {
+        var domElement = document.getElementById('parcel')
+        /*global System*/
+        mountParcel(() => System.import("sample-vue-parcel"), { domElement })
+        //mountRootParcel(() => System.import("sample-react-parcel"), { domElement })
+
+        domElement = document.getElementById('parcel2')
+        /*global System*/
+        mountParcel(() => System.import("vue-parcel2"), { domElement })
+      }
+  }
+}
+</script>
+
+<style scoped>
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 25px;
+}
+</style>
